b5228a66949056da846462a99aa49da07057a5b4
orgamiryazdani <orgamiryazdani@gmail.com>
2024-12-22 20:01:02 +0330

    feat(theme): Add fonts and colors with related configurations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,31 @@
 import type { Config } from "tailwindcss";
 
+export const tailwindColors: { [key: string]: string } = {
+  transparent: "transparent",
+  white: "#ffffff",
+  black: "#000000",
+  "dark-100": "#404258",
+  "dark-200": "#474E68",
+  "dark-300": "#50577A",
+  "dark-400": "#6B728E",
+  "light-100": "#F5EFFF",
+  "light-200": "#E5D9F2",
+  "light-300": "#CDC1FF",
+  "light-400": "#A294F9",
+  success: "#36d399",
+  warning: "#fbbd23",
+  error: "#f87272",
+}
+
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/app/**/*.{tsx,jsx}"],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    colors: tailwindColors,
+    container: {
+      center: true
     },
+    extend: {},
   },
+  darkMode: "class",
   plugins: [],
 } satisfies Config;
