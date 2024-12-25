15dc3eb237382a7f2827f44ae8f73a2fc7493370
orgamiryazdani <orgamiryazdani@gmail.com>
2024-12-25 19:31:35 +0330

    feat: Installing auth.js and configuring login with GitHub

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,21 @@
-export default function Home() {
+'use client';
+import { signIn } from 'next-auth/react';
+import { signOut, useSession } from 'next-auth/react';
+
+export default function Dashboard() {
+  const { data: session } = useSession();
+
   return (
-    <>
-      <div>amir</div>
-      <div>amir</div>
-    </>
+    <div>
+      {session?.user ? (
+        <p>
+          Name : {session.user?.name || 'Unknown'} <br />
+          Email : {session.user?.email || 'No email'} <br />
+          <button onClick={() => signOut()}>SignOUT</button>
+        </p>
+      ) : (
+        <button onClick={() => signIn('github')}>Sign In</button>
+      )}
+    </div>
   );
 }
