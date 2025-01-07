8ae3d5893dace82638ec03d826dade4c5b541387
orgamiryazdani <orgamiryazdani@gmail.com>
2025-01-07 10:43:37 +0330

    fix: Image matcher config

--- src/middleware.ts
+++ src/middleware.ts
@@ -41,6 +41,6 @@ export async function middleware(request: NextRequest) {
 }
 export const config = {
     matcher: [
-        '/((?!_next/static|_next/image|favicon.ico|api/auth|auth|favicon.png|robots.txt|sitemap.xml).*)',
+        '/((?!_next/static|_next/image|favicon.ico|api/auth|auth|favicon.png|robots.txt|sitemap.xml|images).*)',
     ],
 };
