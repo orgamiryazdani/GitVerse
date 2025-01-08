a847c8000913dd31565c64bca0ecf9a8a0dfc5c6
orgamiryazdani <orgamiryazdani@gmail.com>
2025-01-08 19:30:22 +0330

    feat: Using useTheme and completing the program theme change component

--- src/app/[lang]/_components/header/change-theme.tsx
+++ src/app/[lang]/_components/header/change-theme.tsx
@@ -1,8 +1,9 @@
+'use client';
 import { GiUbisoftSun } from 'react-icons/gi';
-import { PiDesktopThin } from 'react-icons/pi';
 import { RiMoonCloudyLine } from 'react-icons/ri';
 import { AnimatePresence } from 'framer-motion';
 import { FramerMotionAnimation } from '../framer-motion';
+import { useTheme } from '@/providers/ThemeProvider';
 
 const moonVariants = {
   hidden: {
@@ -16,7 +17,7 @@ const moonVariants = {
   visible: {
     opacity: 1,
     top: 40,
-    right: -64,
+    right: -40,
     transition: {
       duration: 0.3,
     },
@@ -35,53 +36,43 @@ const sunVariants = {
   visible: {
     opacity: 1,
     top: 40,
-    right: -5,
+    right: 20,
     transition: {
       duration: 0.5,
     },
   },
 };
 
-const desktopVariants = {
-  hidden: {
-    opacity: 0,
-    top: 0,
-    right: 0,
-    transition: {
-      duration: 0.1,
-    },
-  },
-  visible: {
-    opacity: 1,
-    top: 40,
-    right: 50,
-    transition: {
-      duration: 0.7,
-    },
-  },
-};
-
 const ChangeTheme = ({ showTheme, showThemeHandler }: { showTheme: boolean; showThemeHandler: () => void }) => {
+  const { theme, changeTheme } = useTheme();
+
   const icons = [
     { id: 1, name: 'light', icon: <GiUbisoftSun />, variants: sunVariants },
     { id: 2, name: 'dark', icon: <RiMoonCloudyLine />, variants: moonVariants },
-    { id: 3, name: 'system', icon: <PiDesktopThin />, variants: desktopVariants },
-  ];
+  ] as const;
 
   return (
     <div className="relative w-7 flex items-center justify-center">
-      <PiDesktopThin className="text-3xl cursor-pointer absolute" onClick={showThemeHandler} />
+      {theme === 'dark' ? (
+        <RiMoonCloudyLine className="text-3xl cursor-pointer absolute" onClick={showThemeHandler} />
+      ) : (
+        <GiUbisoftSun className="text-3xl cursor-pointer absolute" onClick={showThemeHandler} />
+      )}
       <AnimatePresence>
         {showTheme &&
-          icons.map(({ id, icon, variants }) => (
+          icons.map(({ id, icon, variants, name }) => (
             <FramerMotionAnimation
               key={id}
-              className={`absolute w-10 h-10 bg-dark-400 text-[22px] cursor-pointer flex items-center justify-center rounded-full border-2 border-light-300 text-light-100`}
+              className={`${theme === name ? 'text-light-400 border-light-400' : 'text-light-100 border-light-300'} absolute w-10 h-10 bg-dark-400 text-[22px] cursor-pointer flex items-center justify-center rounded-full border-2`}
               initial="hidden"
               animate="visible"
               exit="hidden"
               tag="div"
               variants={variants}
+              onClick={() => {
+                showThemeHandler();
+                changeTheme(name);
+              }}
             >
               {icon}
             </FramerMotionAnimation>
