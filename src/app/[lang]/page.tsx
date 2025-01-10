c68b9b6e7bce8cd7ed5b34e083dde0860bcddefe
orgamiryazdani <orgamiryazdani@gmail.com>
2025-01-10 18:12:24 +0330

    feat: Add counter component and framer motion for arrow and fix style

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -3,6 +3,23 @@ import { Button } from './_components/button';
 import Image from 'next/image';
 import { getDictionary } from './dictionaries';
 import { lang } from '@/types/languages';
+import { FramerMotionAnimation } from './_components/framer-motion';
+import { Counter } from './_components/counter';
+
+const arrowVariants = {
+  initial: {
+    y: -15,
+  },
+  animate: {
+    y: 0,
+    transition: {
+      duration: 0.5,
+      repeat: Infinity,
+      repeatType: 'reverse' as const,
+      ease: 'easeInOut',
+    },
+  },
+};
 
 type Params = Promise<lang>;
 
@@ -27,7 +44,7 @@ export default async function Home({ params }: { params: Params }) {
     <main className="min-h-[89svh] w-svw flex items-center justify-center flex-col">
       <section className="w-full max-w-8xl min-h-[72vh] flex flex-col lg:flex-row items-center">
         <section
-          className={`lg:w-1/2 w-full lg:h-full min-h-[36vh] flex flex-col items-start lg:justify-center lg:gap-y-7 gap-y-5 ${lang === 'fa' ? 'lg:pr-10' : 'px-10'} p-5 dark:text-white text-black`}
+          className={`lg:w-1/2 w-full lg:h-full min-h-[36vh] flex flex-col items-start lg:justify-center lg:gap-y-7 gap-y-5 ${lang === 'fa' ? 'lg:pr-10' : 'lg:pl-10'} p-5 dark:text-white text-black`}
         >
           <h3 className="dark:text-light-300 text-dark-300 lg:text-sm text-xs">{dict.access_more_features}</h3>
           <h1 className="lg:text-5xl text-2xl font-bold">{dict.platform_title}</h1>
@@ -51,14 +68,31 @@ export default async function Home({ params }: { params: Params }) {
           />
         </section>
       </section>
-      <footer className="w-full min-h-[17vh] flex flex-col md:justify-center">
-        <section className="h-1/2 min-h-[8.5svh] flex flex-col lg:flex-row items-center justify-center lg:gap-20 gap-5 dark:text-light-300 font-bold md:text-2xl text-lg">
-          <span>{dict.free_requests}</span>
-          <span>{dict.premium_requests}</span>
+      <footer className="w-full h-[10vh] md:min-h-[17vh] flex flex-col md:justify-center">
+        <section className="h-1/2 min-h-[50px] flex flex-row items-center justify-center lg:gap-20 gap-5 dark:text-light-300 font-bold md:text-2xl text-sm">
+          <div className="flex items-center justify-center">
+            <div className="md:w-8 w-5">
+              <Counter endNumber={60} />
+            </div>
+            <span className={lang === 'fa' ? 'mb-1' : ''}>{dict.free_requests}</span>
+          </div>
+          <div className="flex items-center justify-center">
+            <div className="flex items-center justify-center md:w-[84px] w-12 gap-x-[1px]">
+              <div className={lang === 'fa' ? 'mb-1' : ''}>+</div>
+              <Counter endNumber={5000} />
+            </div>
+            <span className={lang === 'fa' ? 'mb-1' : ''}>{dict.premium_requests}</span>
+          </div>
         </section>
-        <section className="w-full min-h-[2rem] text-white text-3xl flex items-center justify-center h-1/2 relative">
-          <BsChevronCompactDown className="up-down-icon-animation dark:text-light-100 text-dark-100 absolute" />
-        </section>
+        <FramerMotionAnimation
+          tag="section"
+          initial="initial"
+          animate="animate"
+          variants={arrowVariants}
+          className="w-full min-h-[50px] text-white text-3xl flex items-center justify-center h-1/2 -z-10"
+        >
+          <BsChevronCompactDown className="dark:text-light-100 text-dark-100" />
+        </FramerMotionAnimation>
       </footer>
     </main>
   );
