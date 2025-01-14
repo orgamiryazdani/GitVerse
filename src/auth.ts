5578d204b8fe7db451573957f18a309a7fc810a1
orgamiryazdani <orgamiryazdani@gmail.com>
2025-01-14 23:29:22 +0330

    feat: Setting jwt and session configuration

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,33 @@
 import NextAuth from 'next-auth';
 import GitHubProvider from 'next-auth/providers/github';
+import { UserSession, UserToken } from './types/user.interface';
+
+declare module 'next-auth' {
+  interface User {
+    accessToken: string;
+  }
+
+  interface Session {
+    user: UserSession;
+  }
+
+  interface JWT {
+    user: UserToken;
+  }
+}
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [GitHubProvider],
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.user.accessToken = token.accessToken as string;
+      return session;
+    },
+  },
 });
