759228b79180c92d6ac92a4ffa8bcf66037bdb21
orgamiryazdani <orgamiryazdani@gmail.com>
2025-01-18 18:33:55 +0330

    feat: Get username and set in session

--- src/auth.ts
+++ src/auth.ts
@@ -1,10 +1,13 @@
 import NextAuth from 'next-auth';
 import GitHubProvider from 'next-auth/providers/github';
 import { UserSession, UserToken } from './types/user.interface';
+import axios from 'axios';
+import { API_URL } from './configs/global';
 
 declare module 'next-auth' {
   interface User {
     accessToken: string;
+    username: string;
   }
 
   interface Session {
@@ -22,11 +25,20 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     async jwt({ token, account }) {
       if (account) {
         token.accessToken = account.access_token;
+        const {
+          data: { login },
+        } = await axios.get(`${API_URL}/user`, {
+          headers: {
+            Authorization: `Bearer ${account.access_token}`,
+          },
+        });
+        token.username = login;
       }
       return token;
     },
     async session({ session, token }) {
       session.user.accessToken = token.accessToken as string;
+      session.user.username = token.username as string;
       return session;
     },
   },
