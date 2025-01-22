dab73caa18b19eea9944af6091f7f524e9f2d9b9
orgamiryazdani <orgamiryazdani@gmail.com>
2025-01-22 22:39:43 +0330

    feat: Receiving data from repositories and displaying activities

--- src/app/[lang]/_components/repo-analysis/repo-analysis.tsx
+++ src/app/[lang]/_components/repo-analysis/repo-analysis.tsx
@@ -1,56 +1,88 @@
 'use client';
 import { Button } from '../button';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { IoAnalytics } from 'react-icons/io5';
 import { RiSearch2Fill } from 'react-icons/ri';
 import { useSession } from 'next-auth/react';
-import { readData } from '@/core/http-service';
-import { FaCodeFork, FaRegStar } from 'react-icons/fa6';
-
-interface repoDataType {
-  id: number;
-  name: string;
-  description: string;
-  language: string;
-  forks: number;
-  stargazers_count: number;
-  visibility: string;
-  updated_at: string;
-}
+import { RepoList } from '../repositories';
+import { CommitList } from '../commits';
+import { useGetRepositories } from '@/hooks/useGetRepositories';
+import { useGetCommits } from '@/hooks/useGetCommits';
+import { useGetBranch } from '@/hooks/useGetBranch';
+import toast from 'react-hot-toast';
+import { RepoPlaceholder } from '../placeholders/repo';
+import { CommitsPlaceholder } from '../placeholders/commits';
 
 export const RepoAnalysis: React.FC = () => {
   const session = useSession();
   const [username, setUsername] = useState('');
-  const [repositories, setRepositories] = useState<repoDataType[] | []>([]);
+  const [usernameRepoValue, setUsernameRepoValue] = useState('');
+  const [selectedRepo, setSelectedRepo] = useState({ name: '', owner: '' });
+  const [activeRepo, setActiveRepo] = useState<number | null>(null);
 
-  const getUserRepos = async (ownerUsername?: string) => {
+  const {
+    data: repositories,
+    isLoading: repositoriesLoading,
+    refetch: repositoriesRefetch,
+  } = useGetRepositories(usernameRepoValue);
+
+  const {
+    data: branches,
+    isLoading: branchesLoading,
+    refetch: branchesRefetch,
+  } = useGetBranch(selectedRepo.owner, selectedRepo.name);
+
+  const {
+    data: commits,
+    isLoading: commitsLoading,
+    refetch: commitsRefetch,
+  } = useGetCommits(selectedRepo.owner, selectedRepo.name);
+
+  useEffect(() => {
+    if (repositories.length > 0) {
+      repoHandler(repositories[0].id, repositories[0].owner, repositories[0].name);
+    }
+  }, [repositories]);
+
+  const getUserRepos = async () => {
     const usernameExtraction = username.startsWith('https://github.com/') ? username.split('/')[3] : username;
-    const repository: repoDataType[] = await readData(
-      `/users/${ownerUsername ? ownerUsername : usernameExtraction}/repos`
-    );
-    setRepositories(repository);
+    if (usernameExtraction == '') {
+      toast.error('لطفا مقداری وارد کنید');
+      return;
+    }
+    await setUsernameRepoValue(usernameExtraction);
+    repositoriesRefetch();
   };
 
-  function formatDate(isoDate: string): string {
-    const date = new Date(isoDate);
-    const now = new Date();
-    const diffInMs: number = now.getTime() - date.getTime();
-    const diffInDays: number = Math.floor(diffInMs / (1000 * 60 * 60 * 24));
-
-    if (diffInDays < 7) return `${diffInDays} days ago`;
-    if (diffInDays < 30) {
-      const weeks: number = Math.floor(diffInDays / 7);
-      return `Updated ${weeks} week${weeks > 1 ? 's' : ''} ago`;
+  const getOwnerRepos = async () => {
+    const ownerUsername = session.data?.user.username || '';
+    if (ownerUsername == '') {
+      toast.error('لطفا وارد شوید');
+      return;
     }
+    await setUsernameRepoValue(ownerUsername);
+    repositoriesRefetch();
+  };
 
-    const options: Intl.DateTimeFormatOptions = {
-      month: 'short',
-      day: '2-digit',
-      year: 'numeric',
-    };
+  const getRepoBranches = async () => {
+    branchesRefetch();
+  };
 
-    return `Updated on ${date.toLocaleDateString('en-US', options)}`;
-  }
+  const getCommits = async () => {
+    commitsRefetch();
+  };
+
+  const repoHandler = async (id: number, owner: { login: string }, name: string) => {
+    await setSelectedRepo({
+      name,
+      owner: owner.login,
+    });
+    if (selectedRepo.name !== '' && selectedRepo.owner !== '') {
+      getRepoBranches();
+      getCommits();
+    }
+    setActiveRepo(id);
+  };
 
   return (
     <main className="max-w-8xl w-full h-screen mt-3">
@@ -63,49 +95,25 @@ export const RepoAnalysis: React.FC = () => {
             placeholder="یوزرنیم یا لینک گیت هاب کاربر مورد نظر خود را وارد کنید"
             onChange={(e) => setUsername(e.target.value)}
             type="text"
-            className="w-5/6 h-12 rounded-xl px-3 placeholder:text-sm outline-none focus:ring-4 ring-light-400"
+            className="w-5/6 h-12 rounded-xl px-3 placeholder:text-sm"
           />
-          <Button onClick={() => getUserRepos()} variant="light-400" className="w-1/6 text-xl font-semibold">
+          <Button onClick={getUserRepos} variant="light-400" className="w-1/6 text-xl font-semibold">
             جستجو
             <RiSearch2Fill />
           </Button>
         </div>
-        <Button onClick={() => getUserRepos(session.data?.user.username)} variant="dark-400" className="text-lg w-full">
+        <Button onClick={getOwnerRepos} variant="dark-400" className="text-lg w-full">
           آنالیز صفحه شما
           <IoAnalytics className="text-2xl" />
         </Button>
       </section>
       <section className="flex items-center justify-between mt-5 w-full h-auto px-8 gap-x-7">
-        <div className="w-2/5 h-96 rounded-xl bg-dark-300 border-[10px] border-dark-300 overflow-y-auto flex flex-wrap gap-3">
-          {repositories.map(({ id, name, description, language, forks, stargazers_count, visibility, updated_at }) => (
-            <div key={id} dir="ltr" className="w-full h-auto cursor-pointer bg-dark-400 rounded-2xl ml-1 p-3">
-              <div className="flex items-center gap-x-3">
-                <p className="text-white font-bold">{name}</p>
-                <div className="border border-light-300 text-light-300 text-[11px] flex items-center justify-center h-5 w-12 rounded-2xl">
-                  {visibility}
-                </div>
-              </div>
-              <p className="text-xs text-light-300 mt-1">{description}</p>
-              <div className="flex w-full text-sm gap-x-3 text-light-300 mt-[10px]">
-                <p>{language}</p>
-                {stargazers_count > 0 && (
-                  <p className="flex gap-x-1">
-                    <FaRegStar />
-                    {stargazers_count}
-                  </p>
-                )}
-                {forks > 0 && (
-                  <p className="flex gap-x-1">
-                    <FaCodeFork />
-                    {forks}
-                  </p>
-                )}
-                <p>{formatDate(updated_at)}</p>
-              </div>
-            </div>
-          ))}
-        </div>
-        <div className="w-3/5 h-96 rounded-xl bg-dark-300 border-[10px] border-b-1 border-dark-300 overflow-y-auto flex flex-wrap gap-3"></div>
+        {repositoriesLoading ? (
+          <RepoPlaceholder />
+        ) : (
+          <RepoList repositories={repositories} repoHandler={repoHandler} activeRepo={activeRepo} />
+        )}
+        {commitsLoading ? <CommitsPlaceholder /> : <CommitList branches={branches} commits={commits} />}
       </section>
     </main>
   );
