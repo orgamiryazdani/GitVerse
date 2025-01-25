ca7968d054d030606bf0e23cdcc2fe8f88b73d8e
orgamiryazdani <orgamiryazdani@gmail.com>
2025-01-25 23:48:47 +0330

    feat: Get sha value and add as query

--- src/services/commitsService.ts
+++ src/services/commitsService.ts
@@ -1,5 +1,5 @@
 import { readData } from '@/core/http-service';
 
-export const getCommitsApi = (owner: string, name: string) => {
-  return readData(`/repos/${owner}/${name}/commits?page=1`);
+export const getCommitsApi = (owner: string, name: string, sha?: string) => {
+  return readData(`/repos/${owner}/${name}/commits?page=1&${sha && `sha=${sha}`}`);
 };
