57bd0e0cf3e6a0b7f544a40e3f4a37f09e0a17e9
orgamiryazdani <orgamiryazdani@gmail.com>
2025-01-27 18:21:05 +0330

    feat: Changing the data type that is returned and writing the data type

--- src/services/commitsService.ts
+++ src/services/commitsService.ts
@@ -1,5 +1,15 @@
+import { commitAndPaginationData } from '@/app/[lang]/_components/commits/commit.types';
 import { readData } from '@/core/http-service';
 
-export const getCommitsApi = (owner: string, name: string, sha?: string) => {
-  return readData(`/repos/${owner}/${name}/commits?page=1&${sha && `sha=${sha}`}`);
+type getCommitsApiProps = {
+  owner: string;
+  name: string;
+  sha?: string;
+  page?: number;
+};
+
+export const getCommitsApi = ({ owner, name, sha = '', page = 1 }: getCommitsApiProps) => {
+  return readData<commitAndPaginationData>(`/repos/${owner}/${name}/commits?page=${page}${sha && `&sha=${sha}`}`).then(
+    (data) => data
+  );
 };
