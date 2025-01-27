51ebb01631c4e2a9b31afea26c3bf96aa558d9eb
orgamiryazdani <orgamiryazdani@gmail.com>
2025-01-27 19:32:58 +0330

    feat: Extracting data and values related to pagination from commits

--- src/app/[lang]/_components/commits/commit-list.tsx
+++ src/app/[lang]/_components/commits/commit-list.tsx
@@ -6,22 +6,28 @@ import { useEffect, useState } from 'react';
 import { useGetCommits } from '@/hooks/useGetCommits';
 import { Pagination } from '../pagination';
 
-export const CommitList: React.FC<commitListProps> = ({ commits, branches, selectedRepo, linkHeader, currentPage }) => {
+export const CommitList: React.FC<commitListProps> = ({ commits, branches, selectedRepo }) => {
+  const data: commitDataType[] = commits?.data || [];
   const [commitSearch, setCommitSearch] = useState<commitDataType[] | []>([]);
   const [sha, setSha] = useState('');
   const [page, setPage] = useState(1);
-  const { refetch } = useGetCommits(selectedRepo.owner, selectedRepo.name, sha, page);
+  const { refetch } = useGetCommits({ owner: selectedRepo.owner, name: selectedRepo.name, sha, page });
   const dict = useDictionary();
 
+  const url = commits?.config?.url || '';
+  const pageMatch = url?.match(/page=(\d+)/);
+  const currentPage = pageMatch ? parseInt(pageMatch[1]) : 0;
+  const linkHeader = commits?.headers?.link || '';
+
   useEffect(() => {
-    setCommitSearch(commits);
+    setCommitSearch(data);
   }, [commits]);
 
   const searchHandler = (value: string) => {
     if (value == '') {
-      setCommitSearch(commits);
+      setCommitSearch(data);
     } else {
-      const searchResult = commits.filter((commit) => {
+      const searchResult = data.filter((commit) => {
         return commit.commit.message.toLowerCase().includes(value.toLowerCase());
       });
       setCommitSearch(searchResult);
@@ -44,7 +50,7 @@ export const CommitList: React.FC<commitListProps> = ({ commits, branches, selec
       dir="ltr"
       className="md:w-3/5 w-full h-96 rounded-xl dark:bg-dark-300 bg-light-300 border-[5px] dark:border-dark-300 border-light-300 overflow-y-auto flex flex-wrap gap-3 p-2 pt-0"
     >
-      {commits.length == 0 ? (
+      {data.length == 0 ? (
         <div className="w-full font-semibold text-white h-full text-xl flex gap-y-1 flex-col items-center justify-center">
           <p>{dict.select_a_repository}</p>
           <p>{dict.no_activity_found}</p>
