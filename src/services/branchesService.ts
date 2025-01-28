1d51c8afd165dfc606cd497836d1b329b4ff426e
orgamiryazdani <orgamiryazdani@gmail.com>
2025-01-28 23:35:36 +0330

    feat: Add type and getData query props value

--- src/services/branchesService.ts
+++ src/services/branchesService.ts
@@ -1,5 +1,6 @@
 import { readData } from '@/core/http-service';
+import { branchesAndPaginationData, getBranchesApiProps } from '@/types/branches.type';
 
-export const getBranchesApi = (owner: string, name: string) => {
-  return readData(`/repos/${owner}/${name}/branches`);
+export const getBranchesApi = ({ owner, name, page = 1 }: getBranchesApiProps) => {
+  return readData<branchesAndPaginationData>(`/repos/${owner}/${name}/branches?page=${page}`).then((data) => data);
 };
