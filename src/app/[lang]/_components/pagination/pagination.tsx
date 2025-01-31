f85b0bd1f904369737c48c595ade942cd2a28574
orgamiryazdani <orgamiryazdani@gmail.com>
2025-01-31 11:46:19 +0330

    feat: Import lang and dynamic styles based on lang

--- src/app/[lang]/_components/pagination/pagination.tsx
+++ src/app/[lang]/_components/pagination/pagination.tsx
@@ -1,11 +1,16 @@
 import { MdArrowLeft, MdArrowRight } from 'react-icons/md';
 import { paginationProps } from './pagination.types';
+import { useLang } from '@/providers/language-provider';
 
 export const Pagination: React.FC<paginationProps> = ({ linkHeader, currentPage, paginationHandler }) => {
+  const lang = useLang();
   return (
     <>
       {linkHeader && (
-        <div className="w-44 h-14 sticky left-1/2 -translate-x-1/2 bottom-0 flex items-center justify-center gap-x-3 dark:bg-dark-300 bg-light-400 border dark:border-dark-400 border-light-100 rounded-xl">
+        <div
+          dir="ltr"
+          className={`w-44 h-14 sticky ${lang === 'fa' ? 'right-1/2 translate-x-1/2' : 'left-1/2 -translate-x-1/2'} bottom-0 flex items-center justify-center gap-x-3 dark:bg-dark-300 bg-light-400 border dark:border-dark-400 border-light-100 rounded-xl`}
+        >
           <button
             disabled={currentPage < 2}
             onClick={() => paginationHandler('decrement')}
