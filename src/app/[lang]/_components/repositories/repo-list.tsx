d598675ecc2ddc770cef16e900537ccaccfbfb26
orgamiryazdani <orgamiryazdani@gmail.com>
2025-01-31 11:51:52 +0330

    style: Change repo list style

--- src/app/[lang]/_components/repositories/repo-list.tsx
+++ src/app/[lang]/_components/repositories/repo-list.tsx
@@ -1,4 +1,4 @@
-import { useDictionary } from '@/providers/DictionaryProvider';
+import { useDictionary } from '@/providers/dictionary-provider';
 import { RepoCard } from './repo-card';
 import { repoListProps } from './repo.types';
 import { useGetRepositories } from '@/hooks/useGetRepositories';
@@ -32,10 +32,7 @@ export const RepoList: React.FC<repoListProps> = ({ usernameRepoValue, repoHandl
       {isLoading ? (
         <RepoPlaceholder />
       ) : (
-        <div
-          dir="ltr"
-          className="md:w-2/5 w-full h-96 rounded-xl dark:bg-dark-300 bg-light-300 border-[10px] dark:border-dark-300 border-light-300 overflow-y-auto flex flex-wrap gap-3"
-        >
+        <div className="md:w-2/5 w-full h-96 rounded-xl dark:bg-dark-300 bg-light-300 border-[10px] dark:border-dark-300 border-light-300 overflow-y-auto flex flex-wrap gap-3">
           {repositories.length == 0 ? (
             <div className="flex flex-col gap-y-1 items-center justify-center font-semibold w-full h-full text-white">
               <p>{dict.please_search_for_a_user}</p>
