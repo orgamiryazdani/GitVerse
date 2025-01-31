f4321f33acb2754854a8a9ba7c4a2523f5592d6c
orgamiryazdani <orgamiryazdani@gmail.com>
2025-01-31 11:48:52 +0330

    feat: Add branches placeholder

---
+++ src/app/[lang]/_components/placeholders/branches/branches-placeholder.tsx
@@ -0,0 +1,12 @@
+'use client';
+import React from 'react';
+
+export const BranchesPlaceholder: React.FC = () => {
+  return (
+    <div
+      dir="ltr"
+      role="status"
+      className="animate-pulse w-full md:w-1/3 h-10 dark:bg-dark-400/80 bg-light-200/80 rounded-lg"
+    ></div>
+  );
+};
