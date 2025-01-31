beb5e1cc0bf71839334d4e5c009647ebc449c186
orgamiryazdani <orgamiryazdani@gmail.com>
2025-01-31 11:42:15 +0330

    feat: Adding English and Farsi texts and state related to selectedBranch

--- src/app/[lang]/_components/commits/commit-list.tsx
+++ src/app/[lang]/_components/commits/commit-list.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useDictionary } from '@/providers/DictionaryProvider';
+import { useDictionary } from '@/providers/dictionary-provider';
 import { CommitCard } from './commit-card';
 import { commitDataType, commitListProps } from './commit.types';
 import { useEffect, useState } from 'react';
@@ -15,6 +15,7 @@ export const CommitList: React.FC<commitListProps> = ({ selectedRepo }) => {
   const data: commitDataType[] = commits?.data || [];
   const [commitSearch, setCommitSearch] = useState<commitDataType[] | []>([]);
   const dict = useDictionary();
+  const [selectedBrach, setSelectedBranch] = useState('');
 
   const url = commits?.config?.url || '';
   const pageMatch = url?.match(/page=(\d+)/);
@@ -40,7 +41,8 @@ export const CommitList: React.FC<commitListProps> = ({ selectedRepo }) => {
     await setPage((prevPage) => (direction === 'increment' ? prevPage + 1 : prevPage - 1));
   };
 
-  const filterHandler = async (value: string) => {
+  const filterHandler = async (value: string, name: string) => {
+    await setSelectedBranch(name);
     await setPage(1);
     await setSha(value);
   };
@@ -50,10 +52,7 @@ export const CommitList: React.FC<commitListProps> = ({ selectedRepo }) => {
       {isLoading ? (
         <CommitsPlaceholder />
       ) : (
-        <div
-          dir="ltr"
-          className="md:w-3/5 w-full h-96 rounded-xl dark:bg-dark-300 bg-light-300 border-[5px] dark:border-dark-300 border-light-300 overflow-y-auto flex flex-wrap gap-3 p-2 pt-0"
-        >
+        <div className="md:w-3/5 w-full h-96 rounded-xl dark:bg-dark-300 bg-light-300 border-[5px] dark:border-dark-300 border-light-300 overflow-y-auto flex flex-wrap gap-3 p-2 pt-0">
           {data.length == 0 ? (
             <div className="w-full font-semibold text-white h-full text-xl flex gap-y-1 flex-col items-center justify-center">
               <p>{dict.select_a_repository}</p>
@@ -61,19 +60,25 @@ export const CommitList: React.FC<commitListProps> = ({ selectedRepo }) => {
             </div>
           ) : (
             <>
-              <div className="w-full md:h-14 sticky dark:bg-dark-300 bg-light-300 top-0 flex flex-col gap-y-2 pb-2 pt-1 md:pt-0 md:pb-0 md:flex-row items-center md:gap-x-4 gap-x-2">
+              <div className="w-full xl:h-14 sticky z-10 dark:bg-dark-300 bg-light-300 top-0 flex flex-col gap-y-2 pb-2 pt-1 xl:pt-0 xl:pb-0 xl:flex-row items-center xl:gap-x-4 gap-x-2">
                 <input
                   onChange={(e) => searchHandler(e.target.value)}
                   type="search"
-                  className="md:w-2/3 w-full h-10 rounded-lg px-2 placeholder:text-sm text-white dark:bg-dark-400 bg-light-400 placeholder:text-white"
-                  placeholder="search..."
+                  className="xl:w-2/3 w-full h-10 rounded-lg px-2 placeholder:text-sm text-white dark:bg-dark-400 bg-light-400 placeholder:text-white"
+                  placeholder={dict.search}
                 />
-                <Branches selectedRepo={selectedRepo} filterHandler={filterHandler} />
+                <Branches selectedBrach={selectedBrach} selectedRepo={selectedRepo} filterHandler={filterHandler} />
               </div>
-              <div className="flex flex-col gap-y-4 w-full min-h-56">
-                {commitSearch.map(({ commit, sha, committer }) => (
-                  <CommitCard key={commit.message + sha} commit={commit} committer={committer} />
-                ))}
+              <div dir="ltr" className="flex flex-col gap-y-4 w-full min-h-56">
+                {commitSearch.length == 0 ? (
+                  <p className="w-full h-full dark:text-white flex items-center justify-center text-xl">
+                    {dict.this_result_was_not_found}
+                  </p>
+                ) : (
+                  commitSearch.map(({ commit, sha, committer }) => (
+                    <CommitCard key={commit.message + sha} commit={commit} committer={committer} />
+                  ))
+                )}
               </div>
               <Pagination linkHeader={linkHeader} currentPage={currentPage} paginationHandler={paginationHandler} />
             </>
