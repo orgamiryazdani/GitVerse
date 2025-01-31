fd33a9610a3966048ea832b3045962096a937a51
orgamiryazdani <orgamiryazdani@gmail.com>
2025-01-31 12:30:24 +0330

    style: Fix reponsive style

--- src/app/[lang]/_components/placeholders/commits/commits-placeholder.tsx
+++ src/app/[lang]/_components/placeholders/commits/commits-placeholder.tsx
@@ -7,9 +7,9 @@ export const CommitsPlaceholder: React.FC = () => {
       role="status"
       className="md:w-3/5 w-full h-96 rounded-xl animate-pulse dark:bg-dark-300 bg-light-300 border-[5px] dark:border-dark-300 border-light-300 overflow-y-auto flex flex-wrap gap-3 p-2"
     >
-      <div className="flex w-full h-10 gap-x-4">
-        <div className="h-10 dark:bg-light-100/20 bg-dark-100/20 rounded-lg w-2/3"></div>
-        <div className="h-10 dark:bg-light-100/20 bg-dark-100/20 rounded-lg w-1/3"></div>
+      <div className="flex flex-col lg:flex-row w-full lg:h-10 h-20 gap-x-4 gap-y-4">
+        <div className="h-10 dark:bg-light-100/20 bg-dark-100/20 rounded-lg lg:w-2/3 w-full"></div>
+        <div className="h-10 dark:bg-light-100/20 bg-dark-100/20 rounded-lg lg:w-1/3 w-full"></div>
       </div>
       <div className="h-20 dark:bg-light-100/20 bg-dark-100/20 rounded-lg w-full"></div>
       <div className="h-20 dark:bg-light-100/20 bg-dark-100/20 rounded-lg w-full"></div>
