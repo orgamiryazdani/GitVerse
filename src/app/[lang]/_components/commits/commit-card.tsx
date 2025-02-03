fc7390704dfd6641c24ac1212b6e8c6315114846
orgamiryazdani <orgamiryazdani@gmail.com>
2025-02-03 19:29:04 +0330

    feat: Add show madal handler

--- src/app/[lang]/_components/commits/commit-card.tsx
+++ src/app/[lang]/_components/commits/commit-card.tsx
@@ -2,13 +2,13 @@ import { formatDate } from '@/utils/formatDate';
 import Image from 'next/image';
 import { commitCardProps } from './commit.types';
 
-export const CommitCard: React.FC<commitCardProps> = ({ commit, committer }) => {
+export const CommitCard: React.FC<commitCardProps> = ({ commit, committer, showModal }) => {
   const avatarUrl = committer?.avatar_url || '/images/defaultuser.jpg';
   const committerLogin = committer?.login || 'Unknown User';
   const commitDate = commit?.committer?.date || new Date().toISOString();
 
   return (
-    <div className="border-b dark:border-light-300 border-dark-400 pb-2 cursor-pointer">
+    <div onClick={showModal} className="border-b dark:border-light-300 border-dark-400 pb-2 cursor-pointer">
       <p className="dark:text-white text-dark-100">{commit?.message || 'No commit message available'}</p>
       <div className="flex items-center gap-x-2 text-xs dark:text-white text-dark-300">
         <Image width={18} height={18} className="rounded-full object-contain" src={avatarUrl} alt={committerLogin} />
