94de1239f3fecc69f365fcd6cac575ac9da2cda3
orgamiryazdani <orgamiryazdani@gmail.com>
2025-02-04 19:27:41 +0330

    style: Change modal style and fix reponsive style

--- src/app/[lang]/_components/modal/modal.tsx
+++ src/app/[lang]/_components/modal/modal.tsx
@@ -8,10 +8,13 @@ export const Modal: React.FC<ModalProps> = ({ children, title, open, onClose }) 
     open && (
       <div
         dir="ltr"
-        className="fixed backdrop-blur-sm z-20 w-full h-full bg-light-100/10 top-0 left-0 flex items-center justify-center"
+        className="fixed backdrop-blur-sm z-20 w-full h-full dark:bg-dark-100/70 bg-light-300/70 top-0 left-0 flex items-center justify-center"
       >
-        <div ref={ref} className="w-[95%] h-[95%] bg-dark-400 rounded-md">
-          <div className="w-full border-b min-h-9 h-[6%] flex items-center justify-between px-3 text-xs text-white">
+        <div
+          ref={ref}
+          className="w-[95%] max-h-[95%] relative border-4 dark:border-dark-400 border-light-400 overflow-auto dark:bg-dark-400 bg-light-400 rounded-md"
+        >
+          <div className="w-full sticky top-0 z-20 dark:bg-dark-400 bg-light-400 border-b min-h-9 h-[6%] flex items-center justify-between px-3 text-xs text-white">
             <p>{title}</p>
             <IoCloseCircleOutline className="text-xl cursor-pointer" onClick={onClose} />
           </div>
