46e14fa195a6c95c31b59fe2c7a344471bb82bc9
orgamiryazdani <orgamiryazdani@gmail.com>
2025-02-04 19:25:10 +0330

    feat: Add useGetCommitsActivities custom hook

--- src/hooks/useGetCommits.ts
+++ src/hooks/useGetCommits.ts
@@ -1,5 +1,6 @@
-import { getCommitsApi } from '@/services/commitsService';
-import { commitAndPaginationData, getCommitsApiProps } from '@/types/commits.types';
+import { commitActivityData } from '@/app/[lang]/_components/commits/commit.types';
+import { getCommitActivities, getCommitsApi } from '@/services/commitsService';
+import { activitiesProps, commitAndPaginationData, getCommitsApiProps } from '@/types/commits.types';
 import { useQuery, UseQueryResult } from '@tanstack/react-query';
 
 export const useGetCommits = ({ owner, name, sha, page }: getCommitsApiProps) => {
@@ -12,4 +13,16 @@ export const useGetCommits = ({ owner, name, sha, page }: getCommitsApiProps) =>
   const { data, isLoading, refetch } = queryResult;
 
   return { data, isLoading, refetch };
+};
+
+export const useGetCommitsActivities = ({ repoName, commitSha, owner }: activitiesProps) => {
+  const queryResult: UseQueryResult<commitActivityData> = useQuery({
+    queryKey: ['commits', repoName, commitSha],
+    queryFn: () => getCommitActivities({ repoName, commitSha, owner }),
+    enabled: !!repoName && !!commitSha,
+  });
+
+  const { data, isLoading } = queryResult;
+
+  return { data, isLoading };
 };
