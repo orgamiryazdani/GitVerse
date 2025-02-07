c2f21c3853751cbf7dcf0ae303d528c8d63970ce
orgamiryazdani <orgamiryazdani@gmail.com>
2025-02-07 10:55:37 +0330

    feat: Export email variable

--- src/configs/global.ts
+++ src/configs/global.ts
@@ -1,1 +1,4 @@
 export const API_URL = process.env.NEXT_PUBLIC_API_URL;
+export const SERVICE_ID = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID;
+export const TEMPLATE_ID = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID;
+export const PUBLIC_KEY = process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY;
