e59292ff5686d2904532a84309606ee9f1d46e0e
orgamiryazdani <orgamiryazdani@gmail.com>
2025-02-07 10:59:32 +0330

    style: Fix text size and header height

--- src/app/[lang]/_components/header/header.tsx
+++ src/app/[lang]/_components/header/header.tsx
@@ -11,7 +11,7 @@ export const Header = () => {
 
   return (
     <header
-      className={`dark:text-white w-full h-[11vh] min-h-[70px] md:min-h-0 max-h-[70px] dark:bg-dark-200 bg-light-300 flex transition-all duration-200 ease-in-out ${showMenu ? 'mt-56' : 'mt-0'}`}
+      className={`dark:text-white w-full h-[11vh] min-h-[70px] max-h-[70px] dark:bg-dark-200 bg-light-300 flex transition-all duration-200 ease-in-out ${showMenu ? 'mt-56' : 'mt-0'}`}
     >
       <div className="container w-full flex items-center justify-between max-w-8xl">
         <TopNavigation showMenu={showMenu} showMenuHandler={showMenuHandler} />
