b80ef2220f6ede0318e3a532998aa8477935e352
orgamiryazdani <orgamiryazdani@gmail.com>
2025-02-08 12:47:50 +0330

    feat: Handle react-query error and show error message notification

--- src/lib/react-query.ts
+++ src/lib/react-query.ts
@@ -1,10 +1,30 @@
-import { QueryClient } from '@tanstack/react-query';
+import { MutationCache, QueryCache, QueryClient } from '@tanstack/react-query';
+import { Problem } from '../types/http-errors.interface';
+import toast from 'react-hot-toast';
+
+function isProblem(error: unknown): error is Problem {
+  return (error as Problem).detail !== undefined;
+}
 
 export const queryClient = new QueryClient({
+  queryCache: new QueryCache({
+    onError: (error) => {
+      if (isProblem(error)) {
+        toast.error(error?.detail as string);
+      }
+    },
+  }),
+  mutationCache: new MutationCache({
+    onError: (error: unknown) => {
+      toast.error(error as Problem);
+    },
+  }),
+
   defaultOptions: {
     queries: {
       retry: false,
       refetchOnWindowFocus: false,
+      throwOnError: false,
       gcTime: 1000 * 60 * 60 * 24,
     },
   },
